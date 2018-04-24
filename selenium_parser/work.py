e7e526b87ed41fb32634e6331d6434420b33bda6
rodcoelho <rodcpython@gmail.com>
2018-04-24 11:46:12 -0400

    finished getting each link - time to extract data

--- selenium_parser/work.py
+++ selenium_parser/work.py
@@ -11,10 +11,13 @@ from bs4 import BeautifulSoup
 
 from writeout import write_out_to_log
 
-urlhead = 'https://www.sec.gov/'
+urlhead = 'https://www.sec.gov'
 
 #ciks = ['0001166559', '0001067983']
 ciks = ['0001166559']
+
+thirteen_link = []
+xml_link = []
 
 
 def init_driver():
@@ -28,7 +31,7 @@ def init_driver():
 
 def lookup(driver):
     for cik in ciks:
-        website = '{head}cgi-bin/browse-edgar?CIK={cik}'.format(head=urlhead, cik=cik)
+        website = '{head}/cgi-bin/browse-edgar?CIK={cik}'.format(head=urlhead, cik=cik)
         time.sleep(1)
         driver.get(website)
         timeout = 20
@@ -42,10 +45,7 @@ def lookup(driver):
             payload = elements[0].get_attribute('innerHTML')
             # write_out_to_log(payload=payload, cik_id=cik)
 
-
-            thirteen_link = []
             soup = BeautifulSoup(payload, 'html.parser')
-
 
             rows = soup.findAll('tr')
             for tr in rows:
@@ -53,19 +53,80 @@ def lookup(driver):
 
                 if len(cols) >= 1 and '13F-HR' in cols[0].text:
                     link = cols[1].find('a').get('href')
-                    print(link)
-
-
-
+                    thirteen_link.append(str(link))
         except:
-            print('ERROR')
+            print('ERROR 1')
+
+
+def getxmlpages(driver):
+    for link in thirteen_link:
+        website = '{head}{link}'.format(head=urlhead, link=link)
+        time.sleep(1)
+        driver.get(website)
+        timeout = 20
+
+        try:
+            WebDriverWait(driver, timeout)
+            # driver.implicitly_wait(25)
+
+            # extract data here
+            elements = driver.find_elements_by_class_name('tableFile')
+            payload = elements[0].get_attribute('innerHTML')
+            # write_out_to_log(payload=payload, cik_id=cik)
+
+            soup = BeautifulSoup(payload, 'html.parser')
+
+            rows = soup.findAll('tr')
+            for tr in rows:
+                cols = tr.findAll('td')
+
+                if len(cols) >= 1 and 'INFORMATION TABLE' in cols[1].text:
+                    xlink = cols[2].find('a').get('href')
+                    if 'xslForm' in str(xlink):
+                        xml_link.append(xlink)
+        except:
+            print('ERROR 2')
+
+
+def data_extraction(driver):
+    for link in thirteen_link:
+        website = '{head}{link}'.format(head=urlhead, link=link)
+        time.sleep(1)
+        driver.get(website)
+        timeout = 20
+
+        try:
+            WebDriverWait(driver, timeout)
+            # driver.implicitly_wait(25)
+
+            # extract data here
+            elements = driver.find_elements_by_class_name('tableFile')
+            payload = elements[0].get_attribute('innerHTML')
+            # write_out_to_log(payload=payload, cik_id=cik)
+
+            soup = BeautifulSoup(payload, 'html.parser')
+
+            rows = soup.findAll('tr')
+            for tr in rows:
+                cols = tr.findAll('td')
+
+                if len(cols) >= 1 and 'INFORMATION TABLE' in cols[1].text:
+                    xlink = cols[2].find('a').get('href')
+                    if 'xslForm' in str(xlink):
+                        xml_link.append(xlink)
+        except:
+            print('ERROR 3')
 
 
 if __name__ == "__main__":
     # init driver
     driver = init_driver()
+    # gets the 13F links and stores them in thirteen_link list
     lookup(driver)
+    getxmlpages(driver)
+    data_extraction(driver)
     # close driver
-    time.sleep(1)
+    time.sleep(5)
+    driver.quit()
     driver.quit()
 
